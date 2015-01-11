c9c541119b6a911c16ad5696653f227da94ec1af
Prelang Builder <builder@prelang.com>
2015-01-11 14:33:15 -0500

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150111192924) do
+ActiveRecord::Schema.define(version: 20150111193040) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,5 +45,73 @@ ActiveRecord::Schema.define(version: 20150111192924) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "certifications", force: true do |t|
+    t.string   "name"
+    t.date     "completed_date"
+    t.date     "expiry_date"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "certifications", ["user_id"], name: "index_certifications_on_user_id"
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "companies", ["user_id"], name: "index_companies_on_user_id"
+
+  create_table "roles", force: true do |t|
+    t.string   "type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["user_id"], name: "index_roles_on_user_id"
+
+  create_table "toolbox_talks", force: true do |t|
+    t.integer  "user_id"
+    t.date     "invoice_date"
+    t.string   "attendees"
+    t.string   "topics"
+    t.text     "comments"
+    t.string   "inspections"
+    t.text     "inspection_comments"
+    t.boolean  "confirm"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "toolbox_talks", ["user_id"], name: "index_toolbox_talks_on_user_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.string   "mobile"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
