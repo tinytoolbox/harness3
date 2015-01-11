89db54a61bdc0e3a0e30819c21fca413e971ff89
Prelang Builder <builder@prelang.com>
2015-01-11 19:30:45 +0000

    Installed relation 'has_one' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   
   #->Prelang (user_login:devise/username_login_support)
   has_one :company
+  has_one :role
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
