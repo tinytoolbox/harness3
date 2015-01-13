8a27c6541edbca08f31b00998941815c35512c48
Tom Whitaker <tom@tinytoolbox.ca>
2015-01-13 15:39:23 -0500

    Revised Visual Syling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :login
-  
+
   #->Prelang (user_login:devise/username_login_support)
   has_one :company
   has_one :role
